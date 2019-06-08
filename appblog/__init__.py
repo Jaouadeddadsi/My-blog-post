b53022fd31e2c4859b08630c73ab8a7239741651
Eddadsi <jaouadeddadsi2016@gmail.com>
2019-06-08 16:19:05 +0200

    feat: Finish the registration form

--- appblog/__init__.py
+++ appblog/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ae8f02809892a71a4f9e'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from appblog import routes
